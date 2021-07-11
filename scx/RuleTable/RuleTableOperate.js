79444b256d3b50e3717006ae5477986752e17cd4
scxmonkey <192080084@qq.com>
2021-07-12 00:43:55 +0800

    删除规则库注释说明

--- scx/RuleTable/RuleTableOperate.js
+++ scx/RuleTable/RuleTableOperate.js
@@ -1,9 +1,11 @@
 /*
  * @Author: your name
  * @Date: 2021-07-12 00:24:09
- * @LastEditTime: 2021-07-12 00:32:18
+ * @LastEditTime: 2021-07-12 00:43:11
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \SolidPollutionItem\scx\RuleTable\RuleTableOperate.js
  */
 // TODO: 引入第三方库/资源文件
+
+//TODO: 删除原有规则库
