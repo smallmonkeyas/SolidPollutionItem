700fc3d070db309f9b6e5965cc64dd9a745e0d59
scxmonkey <192080084@qq.com>
2021-07-12 00:28:21 +0800

    建立一个规则库操作文件

---
+++ scx/RuleTable/RuleTableOperate.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-07-12 00:24:09
+ * @LastEditTime: 2021-07-12 00:24:16
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \SolidPollutionItem\scx\RuleTable\RuleTableOperate.js
+ */
